bd5127b9972ee1e7d887cba7e8896a1fa92b3533
kyoung-jnn <rudwls468@gmail.com>
2021-09-15 15:55:03 +0900

    feat: 문제 해결

--- programmers/level2/조이스틱.js
+++ programmers/level2/조이스틱.js
@@ -2,41 +2,42 @@ function solution(name) {
   let answer = 0;
   let currentIndex = 0;
 
+  const ASCII_A = "A".charCodeAt(0);
+  const ASCII_N = "N".charCodeAt(0);
+  const ASCII_Z = "Z".charCodeAt(0);
+
   name = name.split("");
 
   while (true) {
-    // 탈출문
+    // 탈출문, 전부 A 이면 탈출 조건 완료
     if (name.join("") === "A".repeat(name.length)) break;
+    let moveCount = 1;
 
-    let rightIndex = 1;
-    let leftIndex = 1;
-
-    // A가 아니라면 A로 바꾸기
+    // 1. 현재 인덱스가 A 가 아니라면 A 로 바꾸기!
     if (name[currentIndex] !== "A") {
-      if (name[currentIndex].charCodeAt(0) < "N".charCodeAt(0)) {
-        var count = name[currentIndex].charCodeAt(0) - "A".charCodeAt(0);
-      } else {
-        var count = "Z".charCodeAt(0) - name[currentIndex].charCodeAt(0) + 1;
-      }
-      answer += count;
+      name[currentIndex].charCodeAt(0) < ASCII_N
+        ? (answer += name[currentIndex].charCodeAt(0) - ASCII_A)
+        : (answer += ASCII_Z - name[currentIndex].charCodeAt(0) + 1);
+
       name[currentIndex] = "A";
     } else {
-      // A면 "좌 우" 로 A가 아닌걸 찾는다.
+      /*  2. 현재 인덱스가 A 면 "좌, 우"로
+      A 가 아닌 인덱스를 찾아 이동 */
+
       while (true) {
-        if (name[(currentIndex + rightIndex) % name.length] !== "A") {
-          currentIndex += rightIndex;
-          answer += rightIndex;
+        if (name[(currentIndex + moveCount) % name.length] !== "A") {
+          currentIndex += moveCount;
+          answer += moveCount;
           break;
-        } else if (name[currentIndex - leftIndex] !== "A") {
+        } else if (name[currentIndex - moveCount] !== "A") {
           currentIndex =
-            currentIndex < leftIndex
-              ? name.length - (leftIndex - currentIndex)
-              : currentIndex - leftIndex;
-          answer += leftIndex;
+            currentIndex - moveCount < 0
+              ? name.length - (moveCount - currentIndex)
+              : currentIndex - moveCount;
+          answer += moveCount;
           break;
         }
-        rightIndex++;
-        leftIndex++;
+        moveCount++;
       }
     }
   }
