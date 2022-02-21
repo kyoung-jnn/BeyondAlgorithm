33092b40517a29481f20806a861fb3e24efb689d
KyoungJin Roh <kyoung-jnn@adenasoft.com>
2022-02-21 23:34:43 +0900

    feat: 문제 추가

---
+++ inflearn/투포인터, 슬라이딩윈도우, 해쉬/공통원소 구하기.js
@@ -0,0 +1,27 @@
+function solution(arr1, arr2) {
+  let answer = [];
+  arr1.sort();
+  arr2.sort();
+
+  let point1 = 0;
+  let point2 = 0;
+
+  while (point1 < arr1.length && point2 < arr2.length) {
+    if (arr1[point1] === arr2[point2]) {
+      answer.push(arr1[point1]);
+      point1++;
+      point2++;
+    } else if (arr1[point1] > arr2[point2]) {
+      point2++;
+    } else {
+      point1++;
+    }
+  }
+
+  return answer;
+}
+
+const arr1 = [1, 3, 9, 5, 2];
+const arr2 = [3, 2, 5, 7, 8];
+
+console.log(solution(arr1, arr2));
