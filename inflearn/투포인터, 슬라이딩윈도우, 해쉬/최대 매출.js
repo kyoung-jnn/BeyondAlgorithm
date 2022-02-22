edbada53ec6dc5ba2ee5a2518feedd5904677395
KyoungJin Roh <kyoung-jnn@adenasoft.com>
2022-02-22 23:11:52 +0900

    feat: 투 포인터 문제 추가

---
+++ inflearn/투포인터, 슬라이딩윈도우, 해쉬/최대 매출.js
@@ -0,0 +1,20 @@
+function solution(K, arr) {
+  let answer = 0;
+  let sum = 0;
+  for (let i = 0; i < K; i++) {
+    sum += arr[i];
+  }
+  answer = sum;
+
+  for (let right = K; right < arr.length; right++) {
+    const temp = answer + arr[right] - arr[right - K];
+
+    answer = Math.max(temp, answer);
+  }
+
+  return answer;
+}
+
+const arr = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+
+console.log(solution(4, arr));
